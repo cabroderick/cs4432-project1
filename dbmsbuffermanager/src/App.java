1c13108c9874bf300dcd582b56ba04144ac4fe92
cbroderick-PC\cbroderick <sedrahedron@gmail.com>
2021-04-08 12:12:25 -0400

    Wrote input parsing and added project files

--- dbmsbuffermanager/src/App.java
+++ dbmsbuffermanager/src/App.java
@@ -1,8 +1,32 @@
 import java.nio.Buffer;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int frames = Integer.parseInt(args[0]);
         BufferPool pool = new BufferPool(frames);
+        getInput();
+    }
+
+    private static void getInput () {
+        System.out.println("The program is ready for the next command");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String[] sSplit = s.split(" ");
+        switch (sSplit[0]) {
+            case ("GET"):
+                break;
+            case ("SET"):
+                break;
+            case ("PIN"):
+                break;
+            case ("UNPIN"):
+                break;
+            default:
+                System.out.println("The entered command is not valid: please use GET, SET, PIN, OR UNPIN");
+                break;
+        }
+        getInput();
     }
 }
